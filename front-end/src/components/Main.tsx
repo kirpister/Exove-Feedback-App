54cb0a67425042275b4649938cddc43dec9748de
Marina <78799773+marinezh@users.noreply.github.com>
2023-04-21 11:15:39 +0300

    test

--- front-end/src/components/Main.tsx
+++ front-end/src/components/Main.tsx
@@ -1,14 +1,13 @@
-import React from 'react';
-import { Outlet } from 'react-router-dom';
+import React from "react";
+import { Outlet } from "react-router-dom";
 
 const Main: React.FC = () => {
-
-    return (
-        <main>
-            <Outlet />
-            
-        </main>
-    );
+  return (
+    <main>
+      <Outlet />
+      maria
+    </main>
+  );
 };
 
 export default Main;
