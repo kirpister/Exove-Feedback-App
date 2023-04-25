450b52e8ceef7bf6a85b569b67d3b8e699a9a7e1
Marina <78799773+marinezh@users.noreply.github.com>
2023-04-25 23:30:06 +0300

    clean up my branch

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,18 +1,18 @@
 import "./App.css";
-import { Route, Routes, Navigate } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Layout from "./pages/Layout";
 import Login from "./pages/login/Login";
 import Userdash from "./pages/userdash/Userdash";
 import Admindash from "./pages/admindash/Admindash";
+import Form from "./pages/feedback/Form";
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<Login />} />
-        <Route path="/userdash" element={<Userdash />} />
-        <Route path="/admindash" element={<Admindash />} />
-        {/* <Route path="/feedback" element={<Form />} /> */}
+        <Route path="userdash" element={<Userdash />} />
+        <Route path="admindash" element={<Admindash />} />
       </Route>
     </Routes>
   );
--- front-end/src/pages/admindash/Admindash.tsx
+++ front-end/src/pages/admindash/Admindash.tsx
@@ -1,8 +1,6 @@
 import React from "react";
 import SidebarAdmin from "./SidebarAdmin";
 import { useLocation } from "react-router-dom";
-import FeedbackForm1 from "../../components/form/FeedbackForm1";
-import ReviewersConf from "../../components/form/ReviewersConf";
 
 const Admindash: React.FC = () => {
   const { state } = useLocation();
@@ -12,8 +10,6 @@ const Admindash: React.FC = () => {
 
       <div className="dash-wrapper">
         <h2>Welcome {state}</h2>
-        <ReviewersConf />
-        <FeedbackForm1 />
       </div>
     </>
   );
