4ce46a30141b80f824fd6e2db4e0a2943f4bec26
preetiag18 <preetiag18@gmail.com>
2023-04-26 23:52:06 +0300

    Implement validate api

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -8,19 +8,52 @@ import Admindash from "./pages/admindash/Admindash";
 import FeedbackForm1 from "./components/form/FeedbackForm1";
 import Conformation from "./components/form/Conformation";
 import AllFeedbacks from "./components/form/AllFeedbacks";
+import { ADMIN_ROLE } from "./common/constants";
+import { useEffect } from "react";
+import { initiateValidateSession } from "./features/authenticatedUserSlice";
+import { useAppDispatch, useAppSelector } from "./app/hooks";
 
 const App = () => {
-  return (
-    <Routes>
-      <Route path="/" element={<Layout />} />
-      <Route index element={<Login />} />
-      <Route path="userdash" element={<Userdash />} />
-      <Route path="admindash" element={<Admindash />} />
-      <Route path="admindash/feedbackform" element={<FeedbackForm1 />} />
-      <Route path="admindash/conformation" element={<Conformation />} />
-      <Route path="admindash/allfeedbacks" element={<AllFeedbacks />} />
-    </Routes>
-  );
+  const dispatch = useAppDispatch();
+  useEffect(() => {
+    dispatch(initiateValidateSession());
+  }, [dispatch]);
+  const authenticatedUser = useAppSelector((state) => state.authenticatedUser);
+
+  if (authenticatedUser.isLoading) {
+    return <p>Loading....!!</p>;
+  }
+
+  if (authenticatedUser.isLoggedIn) {
+    if (authenticatedUser.userDetails?.roles.includes(ADMIN_ROLE)) {
+      return (
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Admindash />} />
+            <Route path="/feedbackform" element={<FeedbackForm1 />} />
+            <Route path="/conformation" element={<Conformation />} />
+            <Route path="/allfeedbacks" element={<AllFeedbacks />} />
+          </Route>
+        </Routes>
+      );
+    } else {
+      return (
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Userdash />} />
+          </Route>
+        </Routes>
+      );
+    }
+  } else {
+    return (
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Login />} />
+        </Route>
+      </Routes>
+    );
+  }
 };
 
 export default App;
