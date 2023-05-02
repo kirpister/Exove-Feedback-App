cda86c3a3a00df080e6e695ee9a57404951c1881
Marina <78799773+marinezh@users.noreply.github.com>
2023-05-02 03:08:39 +0300

    add components for admin dashboard, styling

---
+++ front-end/src/components/confirm/AllUsersList.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import classes from "./Confirmation.module.css";
+
+const AllUsersList = () => {
+  return (
+    <div className={classes.all_users_list}>
+      <div>
+        <label htmlFor="search"></label>
+        <input type="search" id="search" placeholder="Search"></input>
+      </div>
+      <div>user1</div>
+      <div>user2</div>
+      <div>user3</div>
+    </div>
+  );
+};
+
+export default AllUsersList;
