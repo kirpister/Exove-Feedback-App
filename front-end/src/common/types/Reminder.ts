8bb3936b0e785cb249e752b4be9b9eb1a02adcb8
Preeti Agrawal <preetiag18@gmail.com>
2023-05-17 06:40:28 +0300

    notification to remind all missing users (#102)

---
+++ front-end/src/common/types/Reminder.ts
@@ -0,0 +1,6 @@
+export enum ReminderText {
+  REMIND = "Remind",
+  REMINDER_BEING_SEND = "Sending...",
+  REMINDER_SENT_SUCCESSFULLY = "Reminder sent",
+  REMINDER_SENT_ERROR = "Reminder sending failed...",
+}
